41c83e46cfe6f3a4423dceab84797692d997b995
sb4255 <57270552+sb4255@users.noreply.github.com>
2020-09-27 21:41:45 -0400

    Initial Commit

---
+++ Flixster/MovieCell.swift
@@ -0,0 +1,32 @@
+//
+//  MovieCell.swift
+//  Flixster
+//
+//  Created by Salamata Bah on 9/27/20.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Pods/Target Support Files/Pods-Flixster-FlixsterUITests/Pods-Flixster-FlixsterUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Flixster_FlixsterUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Flixster_FlixsterUITests
+@end
